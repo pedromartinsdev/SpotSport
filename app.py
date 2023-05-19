957aa379ae665c87242dfd33a6f3f647c223058b
Pedro <pedrohenriquemartins@live.com>
2023-05-19 01:03:14 -0300

    add configure page

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from dateutil.parser import parse
 from flask import Flask, render_template, request, redirect, flash, url_for, session
-from sqlalchemy import create_engine, Column, Integer, String, Date, Boolean, ForeignKey
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import create_engine, Column, Integer, String, Date, Boolean, ForeignKey, func, or_
+from sqlalchemy.orm import sessionmaker, relationship, join
 from sqlalchemy.orm import declarative_base
 from werkzeug.debug import DebuggedApplication
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
-
+import json
 
 app = Flask(__name__)
 
@@ -22,15 +22,16 @@ class User(Base, UserMixin):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(80))
+    firstName = Column(String(80))
+    lastName = Column(String(80))
+    photo = Column(String)
+    email = Column(String(200))
+    phone_number = Column(String(20))
     username = Column(String(80), unique=True)
     password = Column(String(80))
-    email = Column(String(200))
-    address = Column(String(200))
     country = Column(String(50))
     birth_date = Column(Date)
     gender = Column(String(10))
-    phone_number = Column(String(20))
     user_verify = Column(Boolean, default=False)
 
     def get_id(self):
@@ -56,8 +57,8 @@ class Event(Base):
     title = Column(String)
     description = Column(String)
     location = Column(String)
-    country = Column(String)
     date = Column(Date)
+    time = Column(String)
     creator_id = Column(Integer, ForeignKey('users.id'))
     user = relationship("User", backref="events")
 
@@ -92,7 +93,7 @@ def login():
         email = request.form['email']
         password = request.form['password']
         user = session.query(User).filter_by(email=email).first()
-        if user and user.password == password:
+        if user and check_password_hash(user.password, password):
             login_user(user)
             flash('Logged in successfully.')
             return redirect(url_for('home'))
@@ -105,16 +106,11 @@ def login():
 @app.route('/logout')
 @login_required
 def logout():
-    session.clear()
-    session.close()
     logout_user()
-    session.pop('usuario', None)
-    print(session)
     return redirect('/login')
 
 
 @app.route('/password', methods=['GET', 'POST'])
-@login_required
 def password():
     if request.method == 'POST':
         email = request.form['email']
@@ -125,75 +121,89 @@ def password():
 @app.route('/home')
 @login_required
 def home():
-    username = current_user.username
     user_id = current_user.id
     myEvents = session.query(Event).filter_by(creator_id=user_id)
     subscribeEvents = session.query(Record).filter_by(user_id=user_id)
 
-    return render_template('home.html', myEvents=myEvents, subscribeEvents=subscribeEvents, username=username)
+    return render_template('home.html', myEvents=myEvents, subscribeEvents=subscribeEvents, user=current_user)
+
 
 @app.route('/records', methods=['GET', 'POST'])
 @login_required
 def records():
-    username = current_user.username
     user_id = current_user.id
     if request.method == 'POST':
         eventId = request.form['id']
         record = Record(user_id=user_id, event_id=eventId)
+
+        row = session.query(Record).filter_by(
+            user_id=user_id, event_id=eventId).all()
+
+        if (len(row) != 0):
+            events = session.query(Event).all()
+            return render_template('event-list.html', events=events, user=current_user)
+
         session.add(record)
         session.commit()
         session.close()
-
-        return render_template('records.html', records=records, username=username)
+        records = session.query(Record).filter_by(user_id=user_id)
+        return render_template('records.html', records=records, user=current_user)
     else:
-        records = session.query(Record).all()
-        return render_template('records.html', records=records, username=username)
+        records = session.query(Record).filter_by(user_id=user_id)
+        return render_template('records.html', records=records, user=current_user)
+
 
 @app.route('/')
 def index():
     if current_user.is_authenticated:
-        username = current_user.username
-        events = session.query(Event).all()
-        return render_template('home.html', events=events, username=username)
+
+        user_id = current_user.id
+        myEvents = session.query(Event).filter_by(creator_id=user_id)
+        subscribeEvents = session.query(Record).filter_by(user_id=user_id)
+
+        return render_template('home.html', myEvents=myEvents, subscribeEvents=subscribeEvents, user=current_user)
     else:
-        print("Não logado")
-        return render_template('login.html')
+        return render_template('landing-page.html')
 
 
 @app.route('/event-list', methods=['GET', 'POST'])
 @login_required
 def event_list():
-    username = current_user.username
     if request.method == 'POST':
         city = request.form['city']
         events = session.query(Event).filter_by(location=city)
-        return render_template('event-list.html', events=events, username=username)
+        return render_template('event-list.html', events=events, user=current_user)
     else:
         events = session.query(Event).all()
-    return render_template('event-list.html', events=events, username=username)
+    return render_template('event-list.html', events=events, user=current_user)
 
 
 @app.route('/user-list', methods=['GET', 'POST'])
 @login_required
 def user_list():
-    username = current_user.username
+    user_id = current_user.id
     if request.method == 'POST':
-        usernameSearch = request.form['username']
-        users = session.query(User).filter_by(username=usernameSearch)
-        return render_template('user-list.html', users=users, username=username)
+        username = request.form['username']
+        users = session.query(User).filter_by(username=username)
+        return render_template('user-list.html', users=users, user=current_user)
     else:
         users = session.query(User).all()
-    return render_template('user-list.html', users=users, username=username)
+        for user in users:
+            if user.id == user_id:
+                index = users.index(user)
+                del users[index]
+    return render_template('user-list.html', users=users, user=current_user)
 
 
 @app.route('/users', methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
-        name = request.form['name']
+        firstName = request.form['first-name']
+        lastName = request.form['last-name']
+        photo =request.form['photo']
         username = request.form['username']
         email = request.form['email']
-        password = request.form['password']
-        address = request.form['address']
+        password = generate_password_hash(request.form['password'])
         country = request.form['country']
         birth_date_str = request.form['birth_date']
         birth_date = parse(birth_date_str).date()
@@ -201,7 +211,7 @@ def create():
         user_verify = False
         phone_number = request.form['phone_number']
 
-        user = User(name=name, username=username, address=address, country=country,
+        user = User(firstName=firstName, lastName=lastName, username=username, photo=photo, country=country,
                     birth_date=birth_date, gender=gender, user_verify=user_verify, phone_number=phone_number, email=email, password=password)
 
         session = Session()
@@ -215,19 +225,18 @@ def create():
 @app.route('/events', methods=['GET', 'POST'])
 @login_required
 def events():
-    username = current_user.username
     if request.method == 'POST':
         title = request.form['title']
         description = request.form['description']
         location = request.form['location']
-        country = request.form['country']
         date_str = request.form['date']
+        time = request.form['time']
         creator_id = current_user.id
 
         date = parse(date_str).date()
 
         event = Event(title=title, description=description, location=location,
-                      country=country, date=date, creator_id=creator_id)
+                      time=time, date=date, creator_id=creator_id)
 
         session.add(event)
         session.commit()
@@ -236,7 +245,16 @@ def events():
 
     events = session.query(Event).all()
     session.close()
-    return render_template('events.html', events=events, username=username)
+    return render_template('events.html', events=events, user=current_user)
+
+
+@app.route('/configure', methods=['GET', 'POST'])
+@login_required
+def configure():
+    if request.method == 'POST':
+        print("daqui a pocuo eu termino")
+    else:
+        return render_template('configure.html', user=current_user)
 
 
 if __name__ == '__main__':
